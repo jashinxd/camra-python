2b104bd24a721dd9665662d471795396013527da
Jason Shin <jasonshin98@gmail.com>
2018-04-12 22:02:19 -0400

    getting songs work

--- app.py
+++ app.py
@@ -11,22 +11,37 @@ def index():
         form = request.form
         print(form)
         selection = form["category"]
+        mood = form["moodoptions"]
         if selection == "weather":
             getWeatherSongs()
         elif selection == "location":
             getLocationSongs()
         elif selection == "mood":
-            getMoodSongs()
+            getSongs(mood)
 
-def getLocationSongs():
+def getLocation():
     url = "http://ipinfo.io/"
     results = requests.get(url).json()
     city = results["city"]
-    getSongs(city)
+    return city
+            
+def getLocationSongs():
+    tag = getLocation()
+    getSongs(tag)
 
-#def getWeatherSongs():
-#    url = 
+def getWeather():
+    city = getLocation()
+    url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=537eb84d28d1b2075c6e44b37f511b10"
+    requested = urllib2.urlopen(url)
+    result = requested.read()
+    r = json.loads(result)
+    weather = r["weather"][0]["main"]
+    return weather
 
+def getWeatherSongs():
+    tag = getWeather()
+    getSongs(tag)
+    
 def getSongs(tag):
     url = "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=" + tag + "&api_key=eaa991e4c471a7135879ba14652fcbe5&format=json"
     requested = urllib2.urlopen(url)
